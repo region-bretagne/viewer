5184303cdb9296ada0f73d53f4c3bcec90391725
spelhate <seb.pelhate@gmail.com>
2015-09-01 16:58:36 +0200

    Add gulp inject

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,14 @@ require('require-dir')('./gulp');
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
+
+var inject = require('gulp-inject');
+ 
+gulp.task('index', function () {
+  var target = gulp.src('./src/index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths: 
+  var sources = gulp.src(['./src/module/*.js', './src/module/*.css'], {read: false});
+ 
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./src'));
+});
