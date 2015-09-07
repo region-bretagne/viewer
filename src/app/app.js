055c92e484a3b63c68c8f3c25011214f5f40e87c
spelhate <seb.pelhate@gmail.com>
2015-09-07 10:55:44 +0200

    Enables config app at startup with a custom js file & add permalink feature

--- src/app/app.js
+++ src/app/app.js
@@ -3,3 +3,32 @@
 //app
 
 var app = angular.module('kartApp', ['ngMaterial']);
+
+app.config(
+    ['$locationProvider', function ($locationProvider) {
+        $locationProvider.html5Mode(true);
+    }]
+);
+
+app.run(function ($http, $rootScope, $location, $mdDialog) {
+    var conf = 'config.json';
+    var params = $location.search();
+    if (params.app) {
+        conf = params.app + '.json';
+    }
+    $http.get(conf)
+        .success(function (data) {
+            $rootScope.config = data;
+            $rootScope.$broadcast('config-loaded');
+        })
+        .error(function (data, status) {
+            $mdDialog.show(
+                $mdDialog.alert()
+                    .parent(angular.element(document.body))
+                    .title('Erreur')
+                    .content(status + ' : ' + data)
+                    .ariaLabel('Alert Dialog')
+                    .ok('Fermer')
+            );
+        });
+});
--- src/app/controllers/mainCtrl.js
+++ src/app/controllers/mainCtrl.js
@@ -1,28 +1,37 @@
 'use strict';
 
-app.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  
-  $scope.toggleSidenav = function(menuID) {
-    $mdSidenav(menuID).toggle();
-    console.log('check');
-  };
+app.controller('AppCtrl', ['$location', '$rootScope', '$scope', '$mdSidenav', 'km_mapMapFactory', function ($location, $rootScope, $scope, $mdSidenav, km_mapMapFactory) {
 
-  $scope.menu = [
-  	{
-  		'title': 'menu 1',
-  		'icon' : 'paper-plane',
-  		'link' : 'url 1'
-  	},
-  	{
-  		'title': 'menu 2',
-  		'icon' : 'folder',
-  		'link' : 'url 2'
-  	},
-  	{
-  		'title': 'menu 3',
-  		'icon' : 'coffee',
-  		'link' : 'url 3'
-  	}
-  ];  
+    //Fire when config.json is loaded
+    $scope.$on('config-loaded', function () {
+        $scope.name = $rootScope.config.name;
+        //get optional url parameters
+        var params = $location.search();
+        if (params.x && params.y && params.z) {
+            //if parameters x,y,z are defined then zoom on this place
+            $rootScope.config.map.center = km_mapMapFactory.fromLonlat([parseFloat(params.x), parseFloat(params.y)]);
+            $rootScope.config.map.zoom = [parseInt(params.z)];
+        }
+        $scope.map = km_mapMapFactory.initMap($rootScope.config.map);
+    });
+
+    $scope.toggleSidenav = function (menuID) {
+        $mdSidenav(menuID).toggle();
+        console.log('check');
+    };
+
+    $scope.menu = [{
+        title: 'menu 1',
+        icon: 'paper-plane',
+        link: 'url 1'
+    }, {
+        title: 'menu 2',
+        icon: 'folder',
+        link: 'url 2'
+    }, {
+        title: 'menu 3',
+        icon: 'coffee',
+        link: 'url 3'
+    }];
 
 }]);
