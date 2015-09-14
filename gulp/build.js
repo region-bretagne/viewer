c2c43fb1022fa7e68bb831c64aa3ae1bc7517d3e
webcrb <webcrb@gmail.com>
2015-09-14 10:50:17 +0200

    module name 'app' & base ref

--- gulp/build.js
+++ gulp/build.js
@@ -35,7 +35,7 @@ gulp.task('partials', function () {
       quotes: true
     }))
     .pipe($.ngHtml2js({
-      moduleName: 'kartennApp'
+      moduleName: 'app'
     }))
     .pipe(gulp.dest('.tmp'))
     .pipe($.size());
